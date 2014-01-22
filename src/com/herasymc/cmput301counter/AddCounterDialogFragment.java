9bbc5f28cfc5ce0cb7f06d58ecef35ef41ab593b
Artem Herasymchuk <artemh@gmail.com>
2014-01-22 01:21:36 -0700

    Finished implementing add dialog

--- src/com/herasymc/cmput301counter/AddCounterDialogFragment.java
+++ src/com/herasymc/cmput301counter/AddCounterDialogFragment.java
@@ -1,5 +1,7 @@
-/* 
- * written using help from the following codepath android tutorial:
+/*
+ * implemented with help from the Android Developer Guides for Dialogs
+ * https://developer.android.com/guide/topics/ui/dialogs.html
+ * and from the CodePath DialogFragment tutorial
  * https://github.com/thecodepath/android_guides/wiki/Using-DialogFragment
  */
 
@@ -10,16 +12,26 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
-public class AddCounterDialogFragment extends DialogFragment {
-
+public class AddCounterDialogFragment extends DialogFragment implements OnEditorActionListener {
+	
+	EditText input;
+	
+	public interface AddCounterDialogListener {
+		void onFinishAddDialog(String inputText);
+	}
+	
 	public AddCounterDialogFragment() {
 		// Empty constructor
 	}
 	
 	public static AddCounterDialogFragment newInstance() {
-		AddCounterDialogFragment fragment  = new AddCounterDialogFragment();
+		AddCounterDialogFragment fragment = new AddCounterDialogFragment();
 		return fragment;
 	}
 	
@@ -28,15 +40,17 @@ public class AddCounterDialogFragment extends DialogFragment {
 		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
 		alertDialogBuilder.setTitle(R.string.dialog_add_title);
 		
-		final EditText input = new EditText(getActivity());
+		input = new EditText(getActivity());
+		input.setHint(R.string.dialog_add_hint);
 		alertDialogBuilder.setView(input);
 		
 		alertDialogBuilder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
 			
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				// TODO Auto-generated method stub
-				
+				AddCounterDialogListener listener = (AddCounterDialogListener) getActivity();
+				listener.onFinishAddDialog(input.getText().toString());
+				dialog.dismiss();
 			}
 		});
 		alertDialogBuilder.setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener() {
@@ -48,6 +62,17 @@ public class AddCounterDialogFragment extends DialogFragment {
 		});
 		
 		return alertDialogBuilder.create();
+	}
+
+	@Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		if (EditorInfo.IME_ACTION_DONE == actionId) {
+			AddCounterDialogListener listener = (AddCounterDialogListener) getActivity();
+			listener.onFinishAddDialog(input.getText().toString());
+			this.dismiss();
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/com/herasymc/cmput301counter/CounterListActivity.java
+++ src/com/herasymc/cmput301counter/CounterListActivity.java
@@ -1,10 +1,15 @@
 package com.herasymc.cmput301counter;
 
+import com.herasymc.cmput301counter.AddCounterDialogFragment.AddCounterDialogListener;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class CounterListActivity extends Activity {
+public class CounterListActivity extends Activity implements AddCounterDialogListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,35 @@ public class CounterListActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.counter_list, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_sort:
+	        	//
+	            return true;
+	        case R.id.action_add:
+	            showAddDialog();
+	            return true;
+	        case R.id.action_summary:
+	        	// Implement summary
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	@Override
+	public void onFinishAddDialog(String inputText) {
+		Toast.makeText(this, "Hi, " + inputText, Toast.LENGTH_SHORT).show();
+	}
+	
+	private void showAddDialog() {
+		FragmentManager manager = getFragmentManager();
+		AddCounterDialogFragment dialog = AddCounterDialogFragment.newInstance();
+		dialog.show(manager, "fragment_add");
 	}
 
 }
