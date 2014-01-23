df51cf4a73689c3169acb210edfc9e8cdd6aadc9
Artem Herasymchuk <artemh@gmail.com>
2014-01-23 12:56:51 -0700

    Move IO out of activity

---
+++ src/com/herasymc/cmput301counter/CounterListIO.java
@@ -0,0 +1,53 @@
+package com.herasymc.cmput301counter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import android.content.Context;
+
+public class CounterListIO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<CounterModel> load(String file, Context context) {
+		ArrayList<CounterModel> l = null;
+		try {
+			FileInputStream f = context.openFileInput(file);
+			ObjectInputStream o = new ObjectInputStream(f);
+			l = (ArrayList<CounterModel>)o.readObject();
+			o.close();
+			f.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if (l == null) {
+			return new ArrayList<CounterModel>();
+		} else {
+			return l;
+		}
+	}
+	
+	public void save(String file, Context context, ArrayList<CounterModel> list) {
+		try {
+			FileOutputStream f = context.openFileOutput(file, Context.MODE_PRIVATE);
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			o.writeObject(list);
+			o.close();
+			f.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
